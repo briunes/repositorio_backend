3c09d796ae262cb69779b24d736b7d6838acdd24
Bruno Barros <team@signed.pt>
2026-07-20 15:19:32 +0100

    Enforce version checks before filter responses

--- src/repo/repo.controller.ts
+++ src/repo/repo.controller.ts
@@ -2,7 +2,6 @@ import {
   Body,
   Controller,
   Get,
-  Header,
   Headers,
   Param,
   Post,
@@ -35,10 +34,6 @@ export class RepoController {
   }
 
   @Get('filters')
-  @Header(
-    'Cache-Control',
-    'public, max-age=0, s-maxage=60, stale-while-revalidate=300',
-  )
   filters() {
     return this.repo.filters();
   }
